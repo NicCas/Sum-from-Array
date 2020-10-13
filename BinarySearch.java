17327b1c43cf6af10eaa4f171b01f8263d01e257
Nicole Casartelli <44043709+NicCas@users.noreply.github.com>
2020-10-12 23:48:38 -0700

    Update BinarySearch.java
    Final *greatly improved* version

--- BinarySearch.java
+++ BinarySearch.java
@@ -7,55 +7,20 @@ public class BinarySearch {
         possibleAddends = userArray;
     }
 
-    public int[] searchSum (int sum, boolean sorted){
-        boolean searchSuccessful;
-
-        // If unsorted then sort it
-        if (!sorted) {
-            quickSort(0, possibleAddends.length-1);
-        }
-
-        if(sum < 0) // If the sum is negative start off by looking for [sum, 0] as the addends, if unsuccessful try [sum+i, -i]
-        {
-            // Do not check duplicates (ie [2,4], [4,2])
-            int cutOff = (int) Math.ceil(sum/2);
-
-            // If (sum + i) exists check if -i exists
-            for (int i = 0; i > cutOff; i--)
-            {
-                searchSuccessful = binSearch(sum - i, 0);
-
-                if(searchSuccessful)
-                {
-                    searchSuccessful = binSearch(i,1);
-                    if (searchSuccessful)
-                        return addends;
-                }
-
-            }
-        } else { // If the sum is positive start off by looking for [sum, 0] as the addends, if unsuccessful try [sum-i, i]
-            int cutOff = (int) Math.floor(sum/2);
-
-            // If (sum - i) exists check if i exists
-            for (int i = 0; i < cutOff; i++)
-            {
-                searchSuccessful = binSearch(sum - i, 0);
-
-                if(searchSuccessful)
-                {
-                    searchSuccessful = binSearch(i,1);
-                    if (searchSuccessful)
-                        return addends;
-                }
-
+    public int[] searchSum (int sum){
+        int checkSum;
+        for (int i = 0; i < possibleAddends.length; i++) {
+            checkSum = sum - possibleAddends[i];
+            if(binSearch(checkSum)){
+                addends[0] = possibleAddends[i];
+                return addends;
             }
         }
-
         return null;
     }
 
     /* Search for provided target # */
-    private boolean binSearch(int target, int index)
+    private boolean binSearch(int target)
     {
         int left = 0, middle;
         int right = possibleAddends.length -1;
@@ -66,7 +31,7 @@ public class BinarySearch {
 
             // If the target is found then return it
             if (possibleAddends[middle] == target){
-                addends[index] = possibleAddends[middle];
+                addends[1] = possibleAddends[middle];
                 return true;
             }
 
@@ -78,37 +43,5 @@ public class BinarySearch {
         }
         return false;
     }
-
-    /* To be called if an unsorted array is passed in */
-    private void quickSort (int low, int high){
-        if (low < high){
-            int index = separate(low, high);
-
-            quickSort(low, index-1);
-            quickSort(index+1, high);
-
-        }
-    }
-
-    private int separate(int low, int high)
-    {
-        int pi = possibleAddends[high];
-        int index = low -1;
-        for (int i = low; i < high - 1; i++) {
-            if(possibleAddends[i] < pi) {
-                index++;
-
-                int temp = possibleAddends[index];
-                possibleAddends[index] = possibleAddends[i];
-                possibleAddends[i] = temp;
-            }
-        }
-        int temp = possibleAddends[index + 1];
-        possibleAddends[index + 1] = possibleAddends[high];
-        possibleAddends[high] = temp;
-
-        return index + 1;
-    }
-
 
 }
